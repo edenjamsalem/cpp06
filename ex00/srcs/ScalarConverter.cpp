bbbee9047258a4f5f7381580aab76eb7131a0e18
Eden <edenbeden98@gmail.com>
2025-03-18 18:13:26 +0000

    ex00: neatened up functions

--- ex00/srcs/ScalarConverter.cpp
+++ ex00/srcs/ScalarConverter.cpp
@@ -100,14 +100,6 @@ scalarType getType(const std::string &str)
 	return (INVALID);
 }
 
-void printOutput(char c, int i, float f, double d)
-{
-	std::cout << "Char: '" << c << "'" << std::endl;
-	std::cout << "Int: " << i << std::endl;
-	std::cout << "Float: " << f << ".0f" << std::endl;
-	std::cout << "Double: " << d << ".0" << std::endl;
-}
-
 void printCustomOutput(std::string c, std::string i, std::string f, std::string d)
 {
 	std::cout << "Char: " << c << std::endl;
@@ -128,12 +120,10 @@ void printSpecial(const std::string &str)
 
 void printChar(const std::string &str)
 {
-	printOutput(
-		static_cast<char>(str[0]),
-		static_cast<int>(str[0]),
-		static_cast<float>(str[0]),
-		static_cast<double>(str[0])
-	);
+	std::cout << "Char: '" << static_cast<char>(str[0]) << "'" << std::endl;
+	std::cout << "Int: " << static_cast<int>(str[0]) << std::endl;
+	std::cout << "Float: " << static_cast<float>(str[0]) << ".0f" << std::endl;
+	std::cout << "Double: " << static_cast<double>(str[0]) << ".0" << std::endl;
 }
 
 void printInt(const std::string &str)
@@ -143,14 +133,12 @@ void printInt(const std::string &str)
 		printCustomOutput("Impossible", "Impossible", ".0f", ".0");
 
 	if (isprint(int(i)))
-		printOutput(static_cast<char>(i), static_cast<int>(i), static_cast<float>(i), static_cast<double>(i));
+		std::cout << "Char: " << static_cast<char>(i) << std::endl;
 	else
-	{
 		std::cout << "Char: " << "Non displayable" << std::endl;
-		std::cout << "Int: " << static_cast<int>(i) << std::endl;
-		std::cout << "Float: " << static_cast<float>(i) << ".0f" << std::endl;
-		std::cout << "Double: " << static_cast<double>(i) << ".0" << std::endl;
-	}
+	std::cout << "Int: " << static_cast<int>(i) << std::endl;
+	std::cout << "Float: " << static_cast<float>(i) << ".0f" << std::endl;
+	std::cout << "Double: " << static_cast<double>(i) << ".0" << std::endl;
 }
 
 void printFloat(const std::string &str)
