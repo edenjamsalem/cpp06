0d97853bbfccc033c9abea69b4b6e7bb8fd0cf49
Eden <edenbeden98@gmail.com>
2025-03-18 17:45:36 +0000

    ex00: mostly working but need to sort decimal printing

--- ex00/srcs/ScalarConverter.cpp
+++ ex00/srcs/ScalarConverter.cpp
@@ -1,4 +1,6 @@
 #include "../includes/ScalarConverter.hpp"
+#include <limits.h>
+#include <cstdlib>
 
 ScalarConverter::ScalarConverter(void) {}
 
@@ -17,15 +19,16 @@ ScalarConverter::~ScalarConverter() {}
 
 bool	isSpecial(const std::string &str)
 {
-	std::string specials[5] = {
+	std::string specials[6] = {
 		"nan",
-		"inf",
+		"nanf",
+		"+inf",
 		"-inf",
-		"inff",
+		"+inff",
 		"-inff"
 	};
 
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < 6; i++)
 	{
 		if (str == specials[i])
 			return (true);
@@ -97,8 +100,114 @@ scalarType getType(const std::string &str)
 	return (INVALID);
 }
 
+void printOutput(char c, int i, float f, double d)
+{
+	std::cout << "Char: '" << c << "'" << std::endl;
+	std::cout << "Int: " << i << std::endl;
+	std::cout << "Float: " << f << ".0f" << std::endl;
+	std::cout << "Double: " << d << ".0" << std::endl;
+}
+
+void printCustomOutput(std::string c, std::string i, std::string f, std::string d)
+{
+	std::cout << "Char: " << c << std::endl;
+	std::cout << "Int: " << i << std::endl;
+	std::cout << "Float: " << f << std::endl;
+	std::cout << "Double: " << d << std::endl;
+}
+
+void printSpecial(const std::string &str)
+{
+	if (str == "nan" || str == "nanf")
+		printCustomOutput("Impossible", "Impossible", "nanf", "nan");
+	if (str == "+inf" || str == "+inff")
+		printCustomOutput("Impossible", "Impossible", "inff", "inf");
+	if (str == "-inf" || str == "-inff")
+		printCustomOutput("Impossible", "Impossible", "-inff", "-inf");
+}
+
+void printChar(const std::string &str)
+{
+	printOutput(
+		static_cast<char>(str[0]),
+		static_cast<int>(str[0]),
+		static_cast<float>(str[0]),
+		static_cast<double>(str[0])
+	);
+}
+
+void printInt(const std::string &str)
+{
+	long i = atol(str.c_str());
+	if (i < INT_MIN || i > INT_MAX)
+		printCustomOutput("Impossible", "Impossible", ".0f", ".0");
+
+	if (isprint(int(i)))
+		printOutput(static_cast<char>(i), static_cast<int>(i), static_cast<float>(i), static_cast<double>(i));
+	else
+	{
+		std::cout << "Char: " << "Non displayable" << std::endl;
+		std::cout << "Int: " << static_cast<int>(i) << std::endl;
+		std::cout << "Float: " << static_cast<float>(i) << ".0f" << std::endl;
+		std::cout << "Double: " << static_cast<double>(i) << ".0" << std::endl;
+	}
+}
+
+void printFloat(const std::string &str)
+{
+	float f = atof(str.c_str());
+
+	if (isprint(int(f)))
+		printOutput(static_cast<char>(f), static_cast<int>(f), static_cast<float>(f), static_cast<double>(f));
+	else
+	{
+		std::cout << "Char: " << "Non displayable" << std::endl;
+		std::cout << "Int: " << static_cast<int>(f) << std::endl;
+		std::cout << "Float: " << f << decimal << "f" << std::endl;
+		std::cout << "Double: " << static_cast<double>(f) << ".0" << std::endl;
+	}
+}
+
+void printDouble(const std::string &str)
+{
+	double f = atof(str.c_str());
+	
+	if (isprint(int(f)))
+		printOutput(static_cast<char>(f), static_cast<int>(f), static_cast<float>(f), static_cast<double>(f));
+	else
+	{
+		std::cout << "Char: " << "Non displayable" << std::endl;
+		std::cout << "Int: " << static_cast<int>(f) << std::endl;
+		std::cout << "Float: " << static_cast<float>(f) << std::endl;
+		std::cout << "Double: " << f << std::endl;
+	}
+}
+
 void ScalarConverter::convert(const std::string &str)
 {
 	scalarType type = getType(str);
-	std::cout << type << std::endl;
+
+	switch (type) 
+	{
+		case INVALID:
+			std::cout << "Invalid input.\n";
+			break ;
+		case SPECIAL:
+			printSpecial(str);
+			break ;
+		case CHAR:
+			printChar(str);
+			break ;
+		case INT:
+			printInt(str);
+			break ;
+		case FLOAT:
+			printFloat(str);
+			break ;
+		case DOUBLE:
+			printDouble(str);
+			break ;
+	}
+
+	//std::cout << type << std::endl;
 }
--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -8,28 +8,48 @@ int main(int argc, char **argv)
         return (1);
     }
     ScalarConverter::convert("inf");
+    std::cout << std::endl;
     ScalarConverter::convert("-inf");
+    std::cout << std::endl;
     ScalarConverter::convert("-inff");
+    std::cout << std::endl;
     ScalarConverter::convert("inff");
+    std::cout << std::endl;
     ScalarConverter::convert("a");
+    std::cout << std::endl;
     ScalarConverter::convert("p");
+    std::cout << std::endl;
     ScalarConverter::convert("z");
+    std::cout << std::endl;
     ScalarConverter::convert("0");
+    std::cout << std::endl;
     ScalarConverter::convert("-0");
+    std::cout << std::endl;
     ScalarConverter::convert("123");
+    std::cout << std::endl;
     ScalarConverter::convert("-123");
+    std::cout << std::endl;
     ScalarConverter::convert("123.123");
+    std::cout << std::endl;
     ScalarConverter::convert("-123.123");
+    std::cout << std::endl;
     ScalarConverter::convert("123.123f");
+    std::cout << std::endl;
     ScalarConverter::convert("-123.123f");
+    std::cout << std::endl;
 
 
     ScalarConverter::convert("0ddwq0");
+    std::cout << std::endl;
     ScalarConverter::convert("123.123.123");
+    std::cout << std::endl;
     ScalarConverter::convert("s123");
+    std::cout << std::endl;
     ScalarConverter::convert("s123.0");
+    std::cout << std::endl;
     ScalarConverter::convert("&");
+    std::cout << std::endl;
     ScalarConverter::convert("aa");
+    std::cout << std::endl;
     ScalarConverter::convert(argv[1]);
-
 }
